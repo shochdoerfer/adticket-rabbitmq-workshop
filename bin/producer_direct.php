babdf1ca254e8720950dc8a6ccdee625ff62e494
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2017-06-20 13:30:03 +0200

    Add STOMP example

---
+++ bin/producer_direct.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../src/bootstrap.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$exchange = 'direct_routing';
+$queue = 'direct_msgs';
+
+$connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+// no need to declare queue and exchange if already defined!
+$channel->queue_declare($queue);
+$channel->exchange_declare($exchange, 'direct');
+$channel->queue_bind($queue, $exchange);
+
+$messageBody = 'Direct connect';
+$message = new AMQPMessage($messageBody);
+$channel->basic_publish($message, $exchange);
+
+$channel->close();
+$connection->close();
